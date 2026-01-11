964c02b2e9249c5e9798ac1c4da7a9ed068cf8ee
VikaGalenco <viktoriygal@mail.ru>
2026-01-11 14:46:41 +0300

    Remove all files, keep only test.py

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Models package
-from .spatial_point import SpatialPoint
-from .material_point import MaterialPoint
-from .trajectory import Trajectory
-from .body import Body
-from .circle_body import CircleBody
-
-__all__ = ['SpatialPoint', 'MaterialPoint', 'Trajectory', 'Body', 'CircleBody']
--- services/__init__.py
+++
@@ -1,9 +0,0 @@
-# Services package
-from .butcher_table import ButcherTable
-from .runge_kutta import RungeKuttaSolver
-from .velocity_field import VelocityField
-from .trajectory_calculator import TrajectoryCalculator
-from .visualization import Visualization
-
-__all__ = ['ButcherTable', 'RungeKuttaSolver', 'VelocityField',
-           'TrajectoryCalculator', 'Visualization']
